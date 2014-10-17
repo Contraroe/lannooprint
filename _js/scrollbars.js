e2cf154091502e5365a1fff022b13c5a0ddea7f7
Pieter De Norre <pieter.de.norre@telenet.be>
2014-10-17 14:14:20 +0200

    opti

---
+++ _js/scrollbars.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(function(){$(".scroll-pane").jScrollPane()})});
---
+++ _js/vacs.js
@@ -0,0 +1,1 @@
+$("#vac_img").click(function(){$("#vac_img").hide(0),$("#vac_cont").animate({right:"0px"}),$("#sluit").show(500),$("#testimonials_cont").animate({right:"-260px"})}),$("#sluit").click(function(){$("#vac_cont").animate({right:"-260px"}),$("#sluit").hide(500),$("#vac_img").show(400),$("#testimonials_cont").animate({right:"0px"})});
