8ce8bcf95b3e6b68dc09260bac4b88d846e4fdd5
Pieter De Norre <pieter.de.norre@telenet.be>
2014-12-05 15:53:58 +0100

    php 5.5 v1.6

--- _includes/testimonials.php
+++ _includes/testimonials.php
@@ -3,25 +3,36 @@
 		<div id="vac_img"></div>
 	</div>
 	<h1 id="testimonials_title">Testimonials</h1>
-	<div id="container_test" class="scroll-pane">
+	<div id="container_test" >
 <?php include '_php/error.php' ?>
 <?php include '_php/db_config.php' ?>
 <?php include '_php/db_connect.php' ?>
 <?php
 
-	$result=mysqli_query($connect,"SELECT * FROM testimonials WHERE testimonials_active = 1 ORDER BY testimonials_date DESC");
-	if (!$result) { die('Invalid query: Line 12' . mysql_error());};
+	$result = mysqli_query($connect, "SELECT * 
+			FROM testimonials 
+			WHERE testimonials_active = 1 
+			ORDER BY testimonials_date 
+			DESC
+		");
+		// if (!$result) { die('Invalid query: Line 12' . mysql_error());};
 
-	$numrows=mysqli_num_rows($result);
-	if (!$numrows) { die('Invalid num_rows: Line 15 ' . mysql_error());};
-	
-	if (!empty($numrows)) {
+		
+			$numa=mysqli_num_rows($result);
+			mysqli_close($connect);
+			if (!empty($numa)) {
+
+
+	// Dbase Content 
+
 		$i=0;
-		while ($i < $numrows) {
-			$f1=html_entity_decode(mysql_result($result,$i,"testimonials_id"));
-			$f2=html_entity_decode(mysql_result($result,$i,"testimonials_klant"));
-			$f3=html_entity_decode(mysql_result($result,$i,"testimonials_date"));
-			$f4=html_entity_decode(mysql_result($result,$i,"testimonials_txt"));
+		while ($i < $numa) {
+			mysqli_data_seek($result,$i);
+			$row=mysqli_fetch_row($result);
+			$f1=html_entity_decode($row[0]);
+			$f2=html_entity_decode($row[1]);
+			$f3=html_entity_decode($row[2]);
+			$f4=html_entity_decode($row[3]);
 ?>
 
 	<div id="testimonials" class="test">
@@ -35,16 +46,15 @@
 		$i++;
 		}
 	}
-	mysqli_close($connect);
 ?>
 <script src="_js/vacs.js"></script>
 </div>
 </div>
 <script type="text/javascript">
-   // $('.testimonialscont:not(:has(div.test))').hide();
-   // $('.testimonialscont:not(:has(div.test))').hide(function(){
-   //       $("#vac_cont").animate({
-   //          right:'0px'
-   //       });   
-   // });
+   $('.testimonialscont:not(:has(div.test))').hide();
+   $('.testimonialscont:not(:has(div.test))').hide(function(){
+         $("#vac_cont").animate({
+            right:'0px'
+         });   
+   });
 </script>
--- _includes/vac.php
+++ _includes/vac.php
@@ -1,42 +1,45 @@
 <div id="vac_cont" class="jobscont">
-    <div id="vac_nav" >
-        <div id="sluit"></div>
-    </div>
-    <h1 id="vac_title">Jobs</h1>
-    <div id="container_vacs" class="scroll-pane">
-<?php include '_php/error.php' ?>
-<?php include '_php/db_config.php' ?>
-<?php include '_php/db_connect.php' ?>
-<?php
-$result=mysqli_query($connect,"
-            SELECT *
-            FROM 'vacatures'
-            WHERE 'vac_active' = 1        
-        ");
-        $numa=mysqli_num_rows($result);
-        // mysqli_close();
-            if (!empty($numa)) {
-    ?>
-    <?php
-        $i=0;
-        while ($i < $numa) {
-            $f1=html_entity_decode(mysql_result($result,$i,"vac_id"));
-            $f2=html_entity_decode(mysql_result($result,$i,"vac_functie"));
-            $f3=html_entity_decode(mysql_result($result,$i,"vac_spec"));
-            $f4=html_entity_decode(mysql_result($result,$i,"vac_txt"));
-?>
+	<div id="vac_nav" >
+		<div id="sluit"></div>
+	</div>
+	<h1 id="vac_title">Jobs</h1>
+	<div id="container_vacs" >
+		<?php include '_php/error.php' ?>
+		<?php include '_php/db_config.php' ?>
+		<?php include '_php/db_connect.php' ?>
+		<?php
+			$result = mysqli_query($connect," SELECT *
+					FROM 'vacatures'
+					WHERE 'vac_active' = 1
+			");
+			
+			$numa = mysqli_num_rows($result);
+			mysqli_close($connect);
+			if (!empty($numa)) {
 
-<div id="vacs" class="job">
+			// Dbase Content 
 
-    <a href="jobs.php?id=<?php echo htmlspecialchars($f1); ?>"><?php echo ($f2); ?><br>
-    <span id="spec"><?php echo htmlspecialchars($f3); ?></span></a>
-</div>
+			$i=0;
+			while ($i < $numa) {
+				mysqli_data_seek($result,$i);
+				$row=mysqli_fetch_row($result);
+				// $f1=html_entity_decode($row[0]);
+				// // $f2=html_entity_decode($row[1]);
+				// // $f3=html_entity_decode($row[2]);
+				// // $f4=html_entity_decode($row[3]);
+		?>
 
-<?php
-    $i++;
-    }
-}
-?>
+		<div id="vacs" class="job">
+
+			<a href="jobs.php?id=<?php echo htmlspecialchars($f1); ?>"><?php echo ($f2); ?><br>
+			<span id="spec"><?php echo htmlspecialchars($f3); ?></span></a>
+		</div>
+
+		<?php
+			$i++;
+			}
+		}
+		?>
 <script src="_js/vacs.js"></script>
 </div>
 </div>
